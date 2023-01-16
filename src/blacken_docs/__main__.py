4db8f9ef671c557c8999e51056d72cd5190e7f33
Adam Johnson <me@adamj.eu>
2023-01-16 10:27:45 +0000

    Standard pre-commit

--- src/blacken_docs/__main__.py
+++ src/blacken_docs/__main__.py
@@ -2,5 +2,5 @@ from __future__ import annotations
 
 from blacken_docs import main
 
-if __name__ == '__main__':  # pragma: no cover
+if __name__ == "__main__":  # pragma: no cover
     raise SystemExit(main())
